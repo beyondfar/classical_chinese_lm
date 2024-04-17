181318dfb1abee27f87d5e76f89c565e916a17b6
zhengshiyou <beyondfar@zhengshiyous-MacBook-Pro.local>
2024-04-17 09:42:12 +0800

    add randomly shuffle and filters out short sentences

--- data/prepare.sh
+++ data/prepare.sh
@@ -2,6 +2,5 @@
 
 #git clone git@github.com:NiuTrans/Classical-Modern.git
 cd Classical-Modern
-find 古文原文 -name "text.txt" | xargs cat > classical_raw.txt
-sed -i .bak 's/\<[^\<\>]*\>//g' classical_raw.txt
+find 古文原文 -name "text.txt" | awk 'BEGIN{srand()}{print rand()"\t"$0}' | sort -r | cut -f2- | xargs cat | sed 's/\<[^\<\>]*\>//g' | awk '{if(length($0)> 10) print}' > classical_raw.txt
 
