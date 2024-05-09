c16631adf66be6664744b624c5e0e37d2eac78a5
zhengshiyou <beyondfar@zhengshiyous-MacBook-Pro.local>
2024-05-09 09:12:08 +0800

    rename to preprocess.sh

--- data/prepare.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#git clone git@github.com:NiuTrans/Classical-Modern.git
-cd Classical-Modern
-find 古文原文 -name "text.txt" | awk 'BEGIN{srand()}{print rand()"\t"$0}' | sort -r | cut -f2- | xargs cat | sed 's/\<[^\<\>]*\>//g' | awk '{if(length($0)> 10) print}' > classical_raw.txt
-
---
+++ data/preprocess.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+git clone git@github.com:NiuTrans/Classical-Modern.git
+python prepare_corpus.py
